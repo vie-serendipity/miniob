501d9a8a0f892f421af72f28ba97476421696cae
vie-serendipity <2733147505@qq.com>
2023-10-21 10:27:50 +0800

    supplement comparison between floats and chars

--- src/observer/sql/parser/value.cpp
+++ src/observer/sql/parser/value.cpp
@@ -213,6 +213,16 @@ int Value::compare(const Value &other) const
   } else if (this->attr_type_ == FLOATS && other.attr_type_ == INTS) {
     float other_data = other.num_value_.int_value_;
     return common::compare_float((void *)&this->num_value_.float_value_, (void *)&other_data);
+  } else if (this->attr_type_ == INTS && other.attr_type_ == CHARS) {
+    float this_data = this->num_value_.int_value_;
+    return common::compare_float_with_string((void *)&this_data, (void *)other.str_value_.c_str());
+  } else if (this->attr_type_ == CHARS && other.attr_type_ == INTS) {
+    float other_data = other.num_value_.int_value_;
+    return common::compare_float_with_string((void *)&other_data, (void *)this->str_value_.c_str());
+  } else if (this->attr_type_ == FLOATS && other.attr_type_ == CHARS) {
+    return common::compare_float_with_string((void *)&this->num_value_.float_value_, (void *)other.str_value_.c_str());
+  } else if (this->attr_type_ == CHARS && other.attr_type_ == FLOATS) {
+    return common::compare_float_with_string((void *)&other.num_value_.float_value_, (void *)this->str_value_.c_str());
   }
   LOG_WARN("not supported");
   return -1;  // TODO return rc?
